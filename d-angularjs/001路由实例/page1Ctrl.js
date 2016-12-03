f0ea83f6928d6b17edde7be0df789485dfbf1c83
jiekekeji <jieke_rose@foxmail.com>
2016-12-03 08:24:03 +0800

    添加page1 控制器逻辑

--- d-angularjs/001路由实例/page1Ctrl.js
+++ d-angularjs/001路由实例/page1Ctrl.js
@@ -3,8 +3,14 @@
 
     //和路由配置的controller: "page1Ctrl"一致
     app.controller("page1Ctrl", androidCtrlFn);
-    function androidCtrlFn($http) {
-        console.log("page1Ctrl");
-
+    function androidCtrlFn($scope, $http) {
+        console.log("page1Ctrl=");
+        $scope.reset = function () {
+            $scope.user = angular.copy($scope.master);
+        };
+        $scope.reset();
+        $scope.submit = function () {
+            console.log("submit master=" + $scope.master.firstName);
+        }
     };
 }());
